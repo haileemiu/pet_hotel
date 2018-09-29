e9e51dee44757a11677819447bb315213f32d71b
Hailee Miu <hailee.miu@gmail.com>
2018-09-29 11:49:57 -0500

    add a nav bar and homepage

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,15 +1,19 @@
 let app = angular.module('PetHotelApp', ['ngRoute', 'ngMaterial']);
 
-app.config(['$routeProvider', function($routeProvider) {
+app.config(['$routeProvider', function ($routeProvider) {
   console.log('Route config it loaded');
 
   $routeProvider
+    .when('/pet_hotel', {
+      templateUrl: 'views/home.html',
+    })
     .when('/pet_hotel/owners', {
       templateUrl: 'views/owner.html',
       controller: 'OwnerController as vm'
     })
     .when('/pet_hotel/pets', {
-      templateUrl: 'views/pet.html', 
+      templateUrl: 'views/pet.html',
       controller: 'PetController as vm'
     })
+    .otherwise( { template: '<h1>404</h1>' });
 }])
